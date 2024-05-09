4fa7265e9064fa344d3aa3e4b5aba247e2a5ae45
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-05-10 01:30:37 +0300

    authclient

---
+++ src/IuliCo.Account/Account/Client/AuthClient.cs
@@ -0,0 +1,39 @@
+﻿
+namespace IuliCo.Account.Account.Cryptography
+{
+    public unsafe class AuthClient
+    {
+        // private ClientWrapper _socket;
+        // public Network.AuthPackets.Authentication Info;
+        // public Account Account;
+        public AuthCryptography Cryptographer;
+        // public int PasswordSeed;
+        // public ConcurrentPacketQueue Queue;
+        public AuthClient()
+        {
+            Cryptographer = new AuthCryptography();
+            // Queue = new ConcurrentPacketQueue(0);
+            // _socket = socket;
+        }
+        public void Send(byte[] buffer)
+        {
+            byte[] _buffer = new byte[buffer.Length];
+            Buffer.BlockCopy(buffer, 0, _buffer, 0, buffer.Length);
+            Cryptographer.Encrypt(_buffer);
+            // _socket.Send(_buffer);
+        }
+        // public string IP
+        // {
+        //     get { return _socket.IP; }
+
+        // }
+        public void Disconnect()
+        {
+            // _socket.Disconnect();
+        }
+        // public void Send(Interfaces.IPacket buffer)
+        // {
+        //     Send(buffer.ToArray());
+        // }
+    }
+}
